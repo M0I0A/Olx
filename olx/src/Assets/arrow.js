ddc0b366c6099bdfda6efb82c3dc3c3f69074b76
Muhammed Irshad Ali K K <75845752+M0I0A@users.noreply.github.com>
2024-07-03 14:05:23 +0530

    created

---
+++ olx/src/Assets/arrow.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function arrow() {
+  return (
+    
+  )
+}
+
+export default arrow
---
+++ olx/src/Components/Header/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './header.css'
+
+function header() {
+  return (
+    <div className='headerParent'>
+        <div className="headerchild">
+            
+        </div>
+      
+    </div>
+  )
+}
+
+export default header
